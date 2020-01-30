2dbe6fbc93b026473826336b0006b4ba611f28ab
Detlef Boehm <drdboehm@gmx.de>
2020-01-30 20:48:44 +0100

    removed error

--- src/main/java/com/example/postgresdemo/controller/UserController.java
+++ src/main/java/com/example/postgresdemo/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
     @Autowired
     UserInstrumentsExperienceRepository userInstrumentsExperienceRepository;
 
-    @Autowire
+    @Autowired
     ExpirienceLevelController expirienceLevelController;
 
 
